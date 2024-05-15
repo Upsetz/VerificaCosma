e2c848b8498b9a15ba69976d5efc7033630d63cd
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 13:14:25 +0200

    verifica

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [partita, setPartita] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [numeroTentato, setNumeroTentato] = useState()
+  const [indovinato, setIndovinato] = useState(false)
+
+  const [risposta, setRisposta] = useState();
+  const [id, setId] = useState();
+  const [rispostaTentativi, setTentativi] = useState();
+
+  async function inizio(){
+    setPartita(true);
+    const response = await fetch("http://localhost:8080/partita", {method: "POST"});
+    const r = await response.json();
+    setPartita(false);
+    setId(r.id);
+    setNumeroTentato(r.numero)
+  }
+
+  async function invio(){
+    setLoading(true);
+    const response = await fetch("http://localhost:8080/partita", 
+        {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"numero" : numeroTentato})
+        });;
+    const r = await response.json();
+    setLoading(false);
+    setTentativi(r.numero);
+    setRisposta(r.numero);
+  }
+
+  function gestisciNumero(e){
+    setNumeroTentato(e.target.value);
+  }
+
+  function addTentativi(){
+    setTentativi(rispostaTentativi + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>INDOVINA NUMERO</h1>
+      <div><button onClick={(inizio)}>nuova partita</button></div>
+      
+      { partita &&
+
+        <div> in caricamento</div>
+      }
+      { !partita && 
+
+        <div> 
+
+          <p>ID: {id}</p>
+          <p>Tentativi: {rispostaTentativi}</p>
+          <p>numero: {numeroTentato}</p>
+
+          
+        {indovinato &&
+          <>
+          <p>indovinato</p>
+          </>
+        }
+        {!indovinato &&
+          <>
+          <p>riprova</p>
+          </>
+        }
+
+          <p>Inserisci un numero da 0 a 100</p>
+          <input type='number' value = {numeroTentato} onClick={invio} onInput={gestisciNumero}></input>
+          <input type='submit' onClick={addTentativi}></input>
+
+        </div>
+
+
+      } 
     </div>
   );
 }
